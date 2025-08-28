d69e062e9c5c8fd1cff1ca747243e84e6c90f5a3
jys0615 <yoonsuh0615@khu.ac.kr>
2025-08-28 18:14:04 +0900

    cleanup: 루트 디렉토리의 중복 Java 프로젝트 파일 제거
    - gradle/, src/ 폴더 제거
    - build.gradle, gradlew, gradlew.bat, settings.gradle 제거
    - backend-server 디렉토리에 통합된 구조 유지

--- src/main/java/kr/ddm/civic/civicdraft/repository/CivicDraftRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ddm.civic.civicdraft.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import kr.ddm.civic.civicdraft.model.CivicDraft;
-
-public interface CivicDraftRepository extends JpaRepository<CivicDraft, Long> {
-}
